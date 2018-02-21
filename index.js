6cc267c68facf5bf5506f2eb54a938894ae6d6ab
Max Lee <maxlee@pacrim.edu>
2018-02-20 20:18:14 -0800

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World i still need Jesus oh thank you for Your blood!');
+}).listen(8080);
