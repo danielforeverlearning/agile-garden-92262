e3345198ecfbd0776e962d551c934ba7a563ca25
Max Lee <maxlee@pacrim.edu>
2018-02-20 20:31:04 -0800

    2nd repair

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World i still need Jesus oh thank you for Your blood!');
-}).listen(8080);
+}).listen(PORT, ()=> console.log(`Listening on ${ PORT }`));
